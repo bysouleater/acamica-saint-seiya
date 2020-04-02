662d9dca7ba2d523e5b6913219a96b141d162e2c
Nahuel Balsamo <nbalsamo@elementum.com>
2020-04-01 21:05:06 -0300

    Server inicial

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = 3000;
+
+server.use(cors());
+
+server.listen(port, function () {
+  console.log('Saint Seiya Server is running on ' + port);
+});
